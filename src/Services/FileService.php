9b3971e3d2b1675e237a192e38493957be07fc19
ManuFuhrmann <manu.koth@arcor.de>
2024-07-01 15:32:44 +0200

    feature: reduce code

--- src/Services/FileService.php
+++ src/Services/FileService.php
@@ -7,32 +7,17 @@ class FileService
     {
         $bytes = floatval($bytes);
         $arBytes = array(
-            0 => array(
-                "UNIT" => "TB",
-                "VALUE" => pow(1024, 4)
-            ),
-            1 => array(
-                "UNIT" => "GB",
-                "VALUE" => pow(1024, 3)
-            ),
-            2 => array(
-                "UNIT" => "MB",
-                "VALUE" => pow(1024, 2)
-            ),
-            3 => array(
-                "UNIT" => "KB",
-                "VALUE" => 1024
-            ),
-            4 => array(
-                "UNIT" => "B",
-                "VALUE" => 1
-            ),
+            "B",
+            "KB",
+            "MB",
+            "GB",
+            "TB",
         );
 
-        foreach($arBytes as $arItem) {
-            if($bytes >= $arItem["VALUE"]) {
-                $result = $bytes / $arItem["VALUE"];
-                $result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
+        foreach($arBytes as $key => $byte) {
+            if($bytes >= pow(1024, $key)) {
+                $result = $bytes / pow(1024, $key);
+                $result = str_replace(".", "," , strval(round($result, 2)))." ".$byte;
                 break;
             } else if ($bytes == 0) {
                 $result = '0 B';
